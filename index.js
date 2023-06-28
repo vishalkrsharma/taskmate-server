febf5e6aa4cb1d64bd8d80b47a529886b16c7d66
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-06-28 16:03:12 +0530

    add client url

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI);
 
