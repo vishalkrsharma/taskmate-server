a161dbd15fc44bef652cfcd5f57a8537179e604e
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-06-29 18:19:46 +0530

    default

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const { login, register } = require('../controllers/userController');
-const allowCors = require('../middlewares/allowCors');
+const { login, register } = require('../controllers/userController'); 
 
 const router = express.Router();
 
-router.post('/login', allowCors, login);
-router.post('/register', allowCors, register);
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
