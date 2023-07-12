3e9d951c6ee11055a859f2942cf14127f520a57d
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-07-12 23:31:45 +0530

    route changed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || process.env.CLIENT_URL_DEV || 'http://localhost:3000',
   })
 );
 app.use(express.json());
